74d0cdd8ef3699daea5fbfb118b646c73903d29c
Rich Hagarty <rich.hagarty@ibm.com>
2018-04-30 09:58:52 -0700

    first pass

--- setup.js
+++ setup.js
@@ -18,10 +18,12 @@
 
 require('dotenv').load({ silent: true });
 
-
 var GDS = require('ibm-graph-client');
 
 console.log("Calling setup script");
+console.log('process.env.VCAP_SERVICES:'+process.env.APP_SERVICES)
+
+
 var config ;
 // Set config
 if (process.env.APP_SERVICES) {
@@ -42,40 +44,60 @@ if (process.env.VCAP_SERVICES) {
   }
 }
 
+console.log('url: ' + config.credentials.apiURL);
+console.log('username: ' + config.credentials.username);
+console.log('password: ' + config.credentials.password);
+
+// https://admin:GVZRJPOPLFHGPIHQ@portal-ssl1412-14.bmix-dal-yp-3ccb4d58-bd36-4d72-8815-d353bb1ce7e6.rich-hagarty-ibm-com.composedb.com:40748/session
+// https://admin:GVZRJPOPLFHGPIHQ@portal-ssl1751-1.bmix-dal-yp-3ccb4d58-bd36-4d72-8815-d353bb1ce7e6.rich-hagarty-ibm-com.composedb.com:40748/session
+
 // Add the graph
+// var graph = new GDS({
+//   url:  config.credentials.apiURL,
+//   username:  config.credentials.username,
+//   password:  config.credentials.password,
+// });
 var graph = new GDS({
-  url:  config.credentials.apiURL,
+  url:  "https://admin:GVZRJPOPLFHGPIHQ@portal-ssl1412-14.bmix-dal-yp-3ccb4d58-bd36-4d72-8815-d353bb1ce7e6.rich-hagarty-ibm-com.composedb.com:40748/session",
   username:  config.credentials.username,
   password:  config.credentials.password,
 });
 
-var gremlin =  require('./data/gremlin.json');
+var gremlin = require('./data/gremlin.json');
 
 // Set Schema
-graph.session(function (token) {
-  graph.config.session = token;
-  console.log("Print the existing Schema");
+graph.session(function (err, token) {
+  if (err) {
+    console.log('Error: ' + err);
+  } else {
+    graph.config.session = token;
 
-  graph.schema().get(function (error, body) {
-    console.log(JSON.stringify(body));
-  });
-  console.log("Set updated schema");
-  var schema = require('./data/schema.json');
-  graph.schema().set(schema, function (error, body) {
-    if (error) {
-      console.log('Error:', error);
-      console.log(body);
-    } else {
-      console.log(body.result.data);
-    }
-    console.log("Creating the Bootgraph Data");
+    const util = require('util');
+    console.log('graph: ');
+    console.log(util.inspect(graph, false, null));
+      
+    console.log("Print the existing Schema");
+
+    graph.schema().get(function (error, body) {
+      console.log(JSON.stringify(body));
+    });
+    console.log("Set updated schema");
+    var schema = require('./data/schema.json');
+    graph.schema().set(schema, function (error, body) {
+      if (error) {
+        console.log('Error:', error);
+        console.log(body);
+      } else {
+        console.log(body.result.data);
+      }
+      console.log("Creating the Bootgraph Data");
       graph.gremlin(gremlin.gremlin.join('\n'), function (e, b) {
         if (e) {
           console.log("Error:",e);
         }
         console.log("Response:",b);
       });
-  });
-
+    });
+  }
 
 });
