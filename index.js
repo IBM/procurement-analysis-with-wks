d1f55c5051871a8d66b6225336cfb589e83b8c90
Ramesh Poomalai <ramesh.poomalai@in.ibm.com>
2018-05-21 16:17:03 +0530

    Updated to clear the errors
    1. Clean up code for errors
    2. Upgraded libs
    3. Fixed service integration issues with latest Watson developer cloud
    apis

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ var Path      = require('path');
 var traversal = require('./app/traversal');
 var syncservice = require('./lib/discoveryServiceHelper');
 const JanusGraphClient = require('./lib/JanusGraphClient');
+
 let graphId = 'procurementsystem';
+
 
 // Handle Configs
 var appEnv = cfenv.getAppEnv();
@@ -37,181 +39,197 @@ let graphClient = new JanusGraphClient(
   process.env.GRAPH_DB_PASSWORD
 );
 
+
+
 var server = new Hapi.Server({
-  debug: {
-    request: ['error', 'good'],
-  },
-  connections: {
     routes: {
       files: {
         relativeTo: Path.join(__dirname, ''),
       },
     },
-  },
+    host: appEnv.bind || process.env.HOST || 'localhost',
+    port: appEnv.port || process.env.PORT || 3000,
 });
 
-// Set Hapi Connections
-server.connection({
-  host: appEnv.bind || process.env.HOST || 'localhost',
-  port: appEnv.port || process.env.PORT || 3000,
-});
 
-// Hapi Log
-server.log(['error', 'database', 'read']);
 
-server.views({
-  engines: { jade: require('pug') },
-  path: __dirname + '/templates',
-});
+const start = async function () {
 
-// Static
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: '.',
-      redirectToSlash: true,
-      index: true,
-    },
-  },
-});
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply.view('index');
-  },
-});
+    await server.register([{        plugin: require('inert')}, {plugin: require('vision') }] );
 
-var gremlinQuery = function (request, reply) {
+    server.views({
+        engines: { jade: require('pug') },
+        relativeTo: __dirname,
+        path: 'templates'
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash: true,
+          index: true,
+        },
+      },
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, reply) {
+        return reply.view('index');
+
+      },
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/knowledgegraph/{querytype}/{showall}',
+      handler: gremlinQuery,
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/knowledgegraph/{querytype}',
+      handler: gremlinQuery,
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/knowledgegraph/{querytype}/{nodeTYpe}/{nodeValue}',
+      handler: gremlinQuery,
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/knowledgegraph/',
+      handler: gremlinQuery,
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/knowledgegraph/syncService',
+      handler: function (request, reply){
+
+
+        syncservice.syncService(function (response) {
+          var responseList = [];
+          responseList.push(response);
+          console.log('Called sync Service..................... 4444'+responseList);
+        });
+
+        return ("called sync Service");
+
+      },
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/knowledgegraph/resetService',
+      handler: function (request, reply){
+        console.log('Called reset Service');
+        var response = [];
+        response[0]='Transient data is cleared..';
+
+        return graphClient.runGremlinQuery(graphId, 'graph.traversal().V().has("type", "transient").drop().iterate(); graph.traversal().tx().commit();')
+          .then((res) => {
+            console.log('Response:' + res);
+
+            return Promise.resolve(response);
+          }).catch(function(rej) {
+            console.log('Error executing the gramlin query..' + rej);
+            return Promise.reject(new Error('Error accessing data'));
+          });
+
+      },
+    });
+
+    /*eslint quotes: ["error", "single", { "avoidEscape": true }]*/
+
+    server.route({
+      method: 'GET',
+      path: '/listCommodity',
+      handler: function (request, reply) {
+        let query = "g.V().hasLabel('Commodity').has('name', 'MMA')";
+        return graphClient.runGremlinQuery(graphId, `def g = graph.traversal(); ${query}`)
+          .then((response) => {
+            var productList = [];
+            if (response.result && response.result.data && response.result.data.length > 0) {
+              console.log('response.result.data:' + JSON.stringify(response.result.data));
+              //var b = JSON.parse(response.result.data);
+              for (var i = 0; i < response.result.data.length; i++) {
+                var product = response.result.data[i];
+                productList.push(product.properties.name[0].value);
+              }
+            }
+            return productList;
+          }).catch(function(rej) {
+            console.log('Error executing the gramlin query.' + rej);
+            return null;
+          });
+
+      },
+    });
+    // Start Hapi
+
+    await server.start(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Server started at: ' + server.info.uri);
+      }
+    });
+    console.log('Server started at: ' + server.info.uri);
+
+};
+
+start();
+
+
+
+var gremlinQuery = function (request, h) {
   var querytype = (request.params.querytype) ? request.params.querytype : 'scheduled_maintenance';
   var queryPath;
   switch (querytype) {
-  case 'scheduled_maintenance':
-    queryPath = traversal.traversal1;
-    break;
-  case 'all_with_full_capacity':
-    queryPath = traversal.traversal2;
-    break;
-  case 'government_regulations':
-    queryPath = traversal.traversal3;
-    break;
-  case 'supply_law':
-    queryPath = traversal.traversal4;
-    break;
-  case 'typhoon':
-    queryPath = traversal.traversal;
-    break;
-  default:
-    queryPath = traversal.traversal;
+      case 'scheduled_maintenance':
+        queryPath = traversal.traversal1;
+        break;
+      case 'all_with_full_capacity':
+        queryPath = traversal.traversal2;
+        break;
+      case 'government_regulations':
+        queryPath = traversal.traversal3;
+        break;
+      case 'supply_law':
+        queryPath = traversal.traversal4;
+        break;
+      case 'typhoon':
+        queryPath = traversal.traversal;
+        break;
+      default:
+        queryPath = traversal.traversal;
 
-  }
-  console.log('Gremlin Query: ' + queryPath.join('.'));
+    }
 
-  graphClient.runGremlinQuery(graphId, 'def g = graph.traversal();' + queryPath.join('.'))
+
+  return graphClient.runGremlinQuery(graphId, 'def g = graph.traversal();' + queryPath.join('.'))
     .then((response) => {
+
       var returnData = {};
       if (response.result && response.result.data && response.result.data.length > 0) {
         returnData.query = queryPath.toString();
         returnData.data = response.result.data;
       }
-      console.log('Gremlin Query Response:');
       // const util = require('util');
       // console.log(util.inspect(response, false, null));
-        
-      reply(returnData);
+      return Promise.resolve(returnData);
     }).catch(function(rej) {
       console.log('Error executing the gramlin query..' + rej);
+      return Promise.reject(new Error('Error accessing data'));
+
     });
+
 };
-
-server.route({
-  method: 'GET',
-  path: '/knowledgegraph/{querytype}/{showall}',
-  handler: gremlinQuery,
-});
-
-server.route({
-  method: 'GET',
-  path: '/knowledgegraph/{querytype}',
-  handler: gremlinQuery,
-});
-
-server.route({
-  method: 'GET',
-  path: '/knowledgegraph/{querytype}/{nodeTYpe}/{nodeValue}',
-  handler: gremlinQuery,
-});
-
-server.route({
-  method: 'GET',
-  path: '/knowledgegraph/',
-  handler: gremlinQuery,
-});
-
-server.route({
-  method: 'GET',
-  path: '/knowledgegraph/syncService',
-  handler: function (request, reply){
-    console.log('Called sync Service');
-    syncservice.syncService(function (response) {
-      var responseList = [];
-      responseList.push(response);
-      reply(responseList);
-    });
-  },
-});
-
-server.route({
-  method: 'GET',
-  path: '/knowledgegraph/resetService',
-  handler: function (request, reply){
-    console.log('Called reset Service');
-    var response = [];
-    response[0]='Transient data is cleared..';
-
-    graphClient.runGremlinQuery(graphId, 'graph.traversal().V().has("type", "transient").drop().iterate();')
-      .then((res) => {
-        console.log('Response:' + res);
-        reply(response);
-      }).catch(function(rej) {
-        console.log('Error executing the gramlin query..' + rej);
-      });
-  },
-});
-
-/*eslint quotes: ["error", "single", { "avoidEscape": true }]*/
-
-server.route({
-  method: 'GET',
-  path: '/listCommodity',
-  handler: function (request, reply) {
-    let query = "g.V().hasLabel('Commodity').has('name', 'MMA')";
-    return graphClient.runGremlinQuery(graphId, `def g = graph.traversal(); ${query}`)
-      .then((response) => {
-        var productList = [];
-        if (response.result && response.result.data && response.result.data.length > 0) {
-          console.log('response.result.data:' + JSON.stringify(response.result.data));
-          //var b = JSON.parse(response.result.data);
-          for (var i = 0; i < response.result.data.length; i++) {
-            var product = response.result.data[i];
-            productList.push(product.properties.name[0].value);
-          }
-        }
-        reply(productList);
-      }).catch(function(rej) {
-        console.log('Error executing the gramlin query..' + rej);
-      });
-  },
-});
-
-// Start Hapi
-server.start(function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Server started at: ' + server.info.uri);
-  }
-});
